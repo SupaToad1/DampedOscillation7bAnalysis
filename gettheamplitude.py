07bd69fbc7971874810f83d99d421bdd4518f36c
W S <117854146+SupaToad1@users.noreply.github.com>
2024-04-24 20:53:23 +0100

    Add files via upload

---
+++ gettheamplitude.py
@@ -0,0 +1,44 @@
+import sys
+from decimal import *
+import time as t
+import numpy as np
+
+
+naturallogampvalues=[] # 2d array containing the time at which a cycle started and the ln value of the amplitude
+
+with open(sys.argv[1], 'r') as f:
+    # get the amplitude value
+    # find the time at which amplitude is at a maximum
+    f.readline() # skip the column names
+    line = f.readline()
+    maxamp=Decimal(0)
+    while line:
+        amplitude=Decimal(line.split(",")[1])
+        time=line.split(",")[0]
+        if amplitude>maxamp:
+            maxamp=Decimal(amplitude)
+        line=f.readline()
+        amplitude=Decimal(line.split(",")[1])
+        if amplitude<maxamp:
+            print(f'\n{time}  {maxamp}\n')
+            if maxamp<0:
+                print('error')
+            else:
+                naturallogampvalues.append((time,maxamp.ln()))
+            minamp = amplitude
+            while True:
+                newamp = Decimal(f.readline().split(",")[1])
+                if minamp<newamp:
+                    maxamp=minamp
+                    del minamp,newamp
+                    line=f.readline()
+                    break
+                else:
+                    minamp=newamp
+                print(".",end='',flush=True)
+                t.sleep(0.005)
+    print(naturallogampvalues)    
+'''
+            print(f'{time} {maxamp}')
+            t.sleep(0.5)
+            f.readline()'''
